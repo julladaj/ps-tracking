58a8181a82fd10843fac108d90aec604338aa5da
Julladaj Bleriot <joe@data-horizon.com>
2022-04-09 21:30:02 +0700

    Added payment manual tab

--- resources/views/meters/include/payment_manual.blade.php
+++ resources/views/meters/include/payment_manual.blade.php
@@ -3,10 +3,10 @@
         <thead>
         <tr>
             <th>ชื่อประเภทงาน</th>
-            <th>กฟภ. ลงทุน (บาท)</th>
-            <th>ทำการ (บาท)</th>
-            <th>ผู้ใช้ไฟ (บาท)</th>
-            <th>รวมทั้งสิ้น (บาท)</th>
+            <th class="text-right">กฟภ. ลงทุน (บาท)</th>
+            <th class="text-right">ทำการ (บาท)</th>
+            <th class="text-right">ผู้ใช้ไฟ (บาท)</th>
+            <th class="text-right">รวมทั้งสิ้น (บาท)</th>
         </tr>
         </thead>
         <tbody>
@@ -48,6 +48,17 @@
         </tr>
         </tbody>
     </table>
+
+    <hr>
+
+    <div class="row mt-1">
+        <div class="col-sm-12 d-flex justify-content-end">
+            <a href="{{ route('meters.index') }}" class="btn btn-secondary mr-1"><i class="bx bx-arrow-back"></i> ย้อนกลับหน้าหลัก</a>
+            <a href="{{ route('meter-pdf', $meter->id) }}" target="_BLANK" class="btn btn-success mr-1"><i class="bx bx-printer"></i> พิมพ์แบบฟอร์ม</a>
+            <button type="reset" class="btn btn-light-secondary mr-1">คืนค่าเริ่มต้น</button>
+            <button type="submit" class="btn btn-primary"><i class="bx bx-save"></i> บันทึกข้อมูล</button>
+        </div>
+    </div>
 </div>
 
 {{-- page scripts --}}
